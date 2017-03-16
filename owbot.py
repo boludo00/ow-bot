3564aa14f8896ccf7a1f42b5ba75c1147e637879
Giancarlo Perrone <gperrone@westhealth.org>
2017-03-15 17:55:57 -0700

    changed core functionality to two main commands for the moment, solving the problem of a global battletag variable.

--- owbot.py
+++ owbot.py
@@ -3,25 +3,23 @@ import requests
 import json
 
 ENDPOINT = "https://enhanced-ow-api.herokuapp.com/"
-BTAG = "(null)"
-G_BTAG = "boludo00-1183" 
 
 my_bot = Bot(command_prefix = "!")
-
 
 @my_bot.event
 async def on_ready():
     print("Client logged in")
 
 @my_bot.command()
-async def hello(*args):
-    return await my_bot.say("hello, world")
-
-@my_bot.command()
 async def h():
-    return await my_bot.say("Available commands:\n\n" \
-                            "!login battle-tag (ex: !login john-1234)\n" \
-                            "!stats hero (Only works if you've logged in. ex: !stats Tracer) \n\n" \
+    return await my_bot.say("Hi! I'm a bot here to provide useful Overwatch information! " \
+                           "Below are some helpful commands and examples on how to use them.\n\n" \
+                           "!stats_comp battletag platform hero\n" \
+                           "\t- Displays all competitive stats for specififc hero in the current Season.\n" \
+                           "\t- Usage: !stats_comp john-0420 pc McCree\n\n"
+                            "!stats_qp battletag platform hero\n" \
+                            "\t- Displays all quickplay stats for specific Hero.\n" \
+                            "\t- Usage: !stats_qp HanzoMain xbox Hanzo\n\n" \
                             "Available heros:\n" \
                             "Reaper\n" 
                             "Tracer\n" \
@@ -49,28 +47,58 @@ async def h():
                             )
 
 @my_bot.command()
-async def login(battletag):
-    global BTAG
-    BTAG = battletag
-    return await my_bot.say("Logged in with: " + battletag)
-
-@my_bot.command()
-async def show():
-    return await my_bot.say("You are currently signed in with: " + BTAG)
-
-@my_bot.command()
-async def stats(hero):
+async def stats_comp(battletag, system, hero):
     """
         Return the stats for requested hero assuming the user has already !login.
         usage:
-            !stats Hanzo
+            !stats john-0420 pc Hanzo
+            !stats HughMungus xbox Hanzo
+            !stats MarcellusWallace ps4 Hanzo
     """
-    url = ENDPOINT + BTAG + "/quickplay/" + "pc/us/"
+    if system == "xbox":
+        platform = "xbl"
+    elif system == "ps4":
+        platform = "ps4"
+    elif system == "pc":
+        platform = "pc/us/"
+    else:
+        return await my_bot.say("Incorrect platform input." \
+                                "Choose any of 'xbox', 'ps4', or 'pc'.")
+         
+    url = ENDPOINT + battletag + "/competetive/" + platform
     print("pinging " + url)
     resp = requests.get(url)
     obj = json.loads(resp.text)
     for cat in obj[hero]:
         await my_bot.say(cat + "\n" + str(json.dumps(obj[hero][cat], indent = 4)).replace("{", "").replace("}", "") + "\n")
 
+@my_bot.command()
+async def stats_qp(battletag, system, hero):
+    """
+        Return the stats for requested hero assuming the user has already !login.
+        usage:
+            !stats john-0420 pc Hanzo
+            !stats HughMungus xbox Hanzo
+            !stats MarcellusWallace ps4 Hanzo
+    """
+    if system == "xbox":
+        platform = "xbl"
+    elif system == "ps4":
+        platform = "ps4"
+    elif system == "pc":
+        platform = "pc/us/"
+    else:
+        return await my_bot.say("Incorrect platform input." \
+                                "Choose any of 'xbox', 'ps4', or 'pc'.")
+         
+    url = ENDPOINT + battletag + "/quickplay/" + platform
+    print("pinging " + url)
+    resp = requests.get(url)
+    obj = json.loads(resp.text)
+    for cat in obj[hero]:
+        await my_bot.say(cat + "\n" + str(json.dumps(obj[hero][cat], indent = 4)).replace("{", "").replace("}", "") + "\n")
 
-my_bot.run("MjkxMDM4MzA2MDYwNDY4MjI0.C6jqSQ.FGHkHtGg8CPRu4W1Os-EbUq48pA")
+with open("info.txt") as f:
+    token = f.read()
+
+my_bot.run(token)
